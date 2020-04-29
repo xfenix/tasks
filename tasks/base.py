9d3035a2d3c098dbaf6d490dfead1fe1f6644e60
Denis Anikin <ad@xfenix.ru>
2020-04-29 03:27:13 +0300

    Basic loop based test + first task

---
+++ tasks/base.py
@@ -0,0 +1,25 @@
+"""Tasks helpers
+"""
+import abc
+import typing
+
+
+class BasicTask(abc.ABC):
+    """Basic task class
+    """
+
+    fixtures: typing.List[typing.Dict[str, typing.Any]] = [
+        dict(input=None, result=None),
+    ]
+
+    def task(self, input_data: typing.Any) -> typing.Any:
+        """Body for task
+        """
+        raise NotImplemented()
+
+    def test_basic(self):
+        """Basic test
+        """
+        for one_test in self.fixtures:
+            fn_result: typing.Any = self.task(one_test['input'])
+            assert fn_result == one_test['result'], f'Expected: {one_test["result"]}, Returned: {fn_result}'
--- tasks/part_1.py
+++ tasks/part_1.py
@@ -2,24 +2,34 @@
 """
 import typing
 
+from . import base
 
-class SortOddNumbersTask:
+
+class SortOddNumbersTask(base.BasicTask):
     """Task #1
 
     Level: easy
     Description: sort array of numbers, where only odd numbers must be sorted
+    Must be in place
     """
 
-    fixtures: typing.Dict = dict(
-        input=[8, 10, 1, 15, 66, -20, 30],
-        result=[],
-    )
+    fixtures: typing.List[typing.Dict[str, typing.List[int]]] = [
+        dict(input=[2, 3, 7, 4, 6, 1, 5, 8, 9], result=[2, 1, 3, 4, 6, 5, 7, 8, 9],),
+        dict(input=[20, 33, 44, 1, 4, -55], result=[20, -55, 44, 1, 4, 33],),
+    ]
 
-    def sort_odd(self, input_array: typing.List) -> typing.List:
+    def task(self, input_array: typing.List[int]) -> typing.List[int]:
+        """Task body
         """
-        """
+        even_array: typing.Iterator[int] = sorted([one_item for one_item in input_array if one_item % 2])
+        for index, one_item in enumerate(input_array):
+            input_array[index] = even_array.pop(0) if one_item % 2 else one_item
+        return input_array
 
-    def test_sort(self):
-        """
-        """
-        assert self.sort_odd(self.fixtures['input']) == self.fixtures['result']
+
+class CompatStringTask:
+    """Task #2
+
+    Level: easy
+    Description: compat string
+    """

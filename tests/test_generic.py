e520d52139254d5bf4fd2946f7d05a305f19f581
Denis Anikin <ad@xfenix.ru>
2020-05-04 18:41:07 +0300

    Code is blacked 😏😏😏 again

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -14,7 +14,7 @@ def test_main_cli(monkeypatch, action_value):
     """Funny __main__.py test.
     We have some difficulties in testing: correctly mock argparse, and reload module. But now its ok
     """
-    module_path: str = 'tasks.__main__'
+    module_path: str = "tasks.__main__"
     need_reload: bool = module_path in sys.modules
     fake_parser_inner: mock.Mock = mock.Mock(parse_args=mock.Mock(return_value=mock.Mock(action=action_value)))
     monkeypatch.setattr("argparse.ArgumentParser", mock.Mock(return_value=fake_parser_inner))
@@ -28,6 +28,7 @@ def test_base(monkeypatch):
     """Test for base.py.
     """
     from tasks import base
+
     new_class: base.BasicTask = base.BasicTask()
     with pytest.raises(NotImplementedError):
         new_class.task(random.randrange(10 ** 2, 10 ** 4))

c2ce37e23e2975949cb2185eaf450caa5c399640
Denis Anikin <ad@xfenix.ru>
2020-05-04 17:46:32 +0300

    Navigation with lines numbers

--- tasks/__main__.py
+++ tasks/__main__.py
@@ -15,18 +15,16 @@ cli_arguments: argparse.Namespace = parser_inst.parse_args()
 
 if cli_arguments.action == 'list':
     docs_map: typing.Dict = {}
-    inner_key: str
-    prepared_doc: str
     for one_mod_path in current_dir.glob('part_*.py'):
         if one_mod_path.is_file():
             one_module: types.ModuleType = importlib.import_module(f'.{one_mod_path.stem}', package='tasks')
             classes_list: typing.List = inspect.getmembers(one_module, inspect.isclass)
             for class_title, one_class_obj in classes_list:
                 if class_title.endswith('Task'):
-                    inner_key = one_class_obj.__doc__.split('\n')[0].rstrip('.').lstrip('Task #')
-                    prepared_doc = one_class_obj.__doc__.replace(' ' * 4, '').rstrip('\n')
-                    prepared_doc = f'{prepared_doc}\n\nWhere: {one_mod_path}'
-                    docs_map[inner_key] = prepared_doc
+                    first_line: int = inspect.getsourcelines(one_class_obj)[1]
+                    inner_key: str = inspect.getdoc(one_class_obj).split('\n')[0].rstrip('.').lstrip('Task #')
+                    docs_map[inner_key] =\
+                        f"{inspect.getdoc(one_class_obj).replace(' ' * 4, '')}\n\nWhere: {one_mod_path}:{first_line}"
     for one_key in sorted(docs_map.keys()):
         print(f'{docs_map[one_key]}\n{"="*30}')
 else:

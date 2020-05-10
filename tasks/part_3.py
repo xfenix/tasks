0c241550a587f1c4b0fed4e3f9f21c47797ac516
Denis Anikin <ad@xfenix.ru>
2020-05-10 03:05:25 +0300

    New tasks, one done, another not

--- tasks/part_3.py
+++ tasks/part_3.py
@@ -38,8 +38,83 @@ class GetNthFromLinkedListEndTask(base.BasicTask):
         ),
     )
     def test_basic(
-        self, one_case: typing.Dict[str, typing.Union[int, None, typing.Dict[str, typing.Union[int, base.LinkedListNode]]]]
+        self,
+        one_case: typing.Dict[str, typing.Union[int, None, typing.Dict[str, typing.Union[int, base.LinkedListNode]]]],
     ) -> None:
         """Basic test.
         """
         assert self.task(one_case["input"]["list"], one_case["input"]["nth"]) == one_case["result"]
+
+
+class AddOneTooArrayOfIntegersTask(base.BasicTask):
+    """Task #11.
+
+    Level: easy
+    Description: given array of integers [1, 2, 3, 4], add 1 to last digit and return
+                 array, as if input was regular integer and output like: [1, 2, 3, 5]
+    """
+
+    fixtures: typing.List[typing.Dict[str, typing.List[int]]] = [
+        # dict(input=[5, 4, 3, 2], result=[5, 4, 3, 3],),
+    ]
+
+    def task(self, input_array: typing.List[int]) -> typing.List[int]:
+        """Task body.
+        TODO: wip
+        """
+        reverse_index: int = 0
+        need_to_increase_next: bool = True
+        for index in range(len(input_array)):
+            reverse_index = len(input_array) - index
+            if input_array[reverse_index] == 9:
+                input_array[reverse_index] = 0
+                need_to_increase_next = True
+                continue
+            if need_to_increase_next:
+                input_array[reverse_index] += 1
+                need_to_increase_next = False
+                continue
+            break
+        return input_array
+
+
+class OverlappingRectanglesTask(base.BasicTask):
+    """Task #12.
+
+    Level: easy
+    Description: given two tuples with two pairs of X, Y coords, which represents
+                 left bottom and right top cornes of rectangles. Need to find overlap square
+    """
+
+    fixtures: typing.List[
+        typing.Dict[str, typing.Union[typing.Tuple[typing.Tuple[int]], typing.Union[bool, int]]]
+    ] = [
+        dict(input=((2, 1), (5, 5), (3, 2), (5, 7)), result=6,),
+        dict(input=((1, 1), (3, 3), (10, 20), (50, 70)), result=False,),
+        dict(input=((-10, -10), (-20, -20), (-5, -5), (-15, -15)), result=25,),
+        dict(input=((-5, -1), (10, 10), (6, 6), (20, 31)), result=16,),
+        dict(input=((-100, -1000), (-10, -10), (-15, -11), (100, 30)), result=5,),
+    ]
+
+    def task(self, input_struct: typing.Tuple[typing.Tuple[int]]) -> typing.Union[bool, int]:
+        """Task body.
+        TODO: wip
+        """
+        def search_2d_intersection(is_x: bool) -> int:
+            """Helper function for intersection search by coord index
+            """
+            index: int = 0 if is_x else 1
+            first_pair_max: int = max(input_struct[0][index], input_struct[1][index])
+            second_pair_min: int = min(input_struct[2][index], input_struct[3][index])
+            if first_pair_max > second_pair_min:
+                return first_pair_max - second_pair_min
+            else:
+                return 0
+
+        x_dist: int = search_2d_intersection(is_x=True)
+        if not x_dist:
+            return False
+        y_dist: int = search_2d_intersection(is_x=False)
+        if not y_dist:
+            return False
+        return x_dist * y_dist
